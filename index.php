7b159720fb6c37f5cab0c5d99b21d1523b9fc4b1
Shakir Hanif <shakirhanif@hotmail.com>
2023-03-01 05:04:33 +0500

    using axios

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
     </form>
     <br><br>
     <button id='getUsers'>GET USERS</button>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.4/axios.min.js" integrity="sha512-LUKzDoJKOLqnxGWWIBM4lzRBlxcva2ZTztO8bTcWPmDSpkErWx0bSP4pdsjNH8kiHAUPaT06UXcb+vOEZH+HpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
+    <script src="axios.js"></script>
     <script src="myjs.js"></script>
 </body>
 </html>
--- myjs.js
+++ myjs.js
@@ -22,24 +22,44 @@ function formSubmit(e) {
 
 document.getElementById("getUsers").addEventListener("click", getUsers);
 var cond = true;
-function getUsers() {
-  var rr = new XMLHttpRequest();
-  rr.open("POST", "user.php", true);
-  rr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-  rr.onload = function () {
-    if (this.status == 200 && cond) {
-      let res = JSON.parse(this.responseText);
-      for (let i = 0; i < res.length; i++) {
-        const username = res[i].username;
-
-        var newP = document.createElement("div");
-        newP.setAttribute("id", username);
-        newP.appendChild(document.createTextNode(username));
-        var form = document.getElementById("getUsers");
-        document.body.insertBefore(newP, form.nextSibling);
-      }
+async function getUsers() {
+  try {
+    const { data, status } = await axios.post("user.php", "getusers");
+    if (status == 200 && cond) {
       cond = false;
+      data.forEach((x) => {
+        const newEl = document.createElement("div");
+        newEl.setAttribute("id", x.username);
+        newEl.appendChild(document.createTextNode(""));
+        newEl.innerHTML = "<p class='new'>" + x.username + "</p>";
+        document.body.insertBefore(
+          newEl,
+          document.getElementById("getUsers").nextSibling
+        );
+      });
     }
-  };
-  rr.send("getusers");
+  } catch (error) {
+    console.log(error);
+  }
 }
+// function getUsers() {
+//   var rr = new XMLHttpRequest();
+//   rr.open("POST", "user.php", true);
+//   rr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+//   rr.onload = function () {
+//     if (this.status == 200 && cond) {
+//       let res = JSON.parse(this.responseText);
+//       for (let i = 0; i < res.length; i++) {
+//         const username = res[i].username;
+
+//         var newP = document.createElement("div");
+//         newP.setAttribute("id", username);
+//         newP.appendChild(document.createTextNode(username));
+//         var form = document.getElementById("getUsers");
+//         document.body.insertBefore(newP, form.nextSibling);
+//       }
+//       cond = false;
+//     }
+//   };
+//   rr.send("getusers");
+// }
